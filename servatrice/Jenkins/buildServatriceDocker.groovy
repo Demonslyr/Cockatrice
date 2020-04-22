75a795a248f92195f5c45aa865e51fb271b47160
Demonslyr <Demonslyr@users.noreply.github.com>
2020-04-22 12:16:15 -0500

    add sendgrid template

--- servatrice/Jenkins/buildServatriceDocker.groovy
+++ servatrice/Jenkins/buildServatriceDocker.groovy
@@ -15,10 +15,12 @@ node {
     }
     stage('build'){
         withCredentials([usernamePassword(usernameVariable: "servatriceUser",passwordVariable: "servatricePass", credentialsId: servatriceCredId)]){
-            def configReplaceOut = sh(returnStdout: true, script: "export DATABASE_USER=${servatriceUser} && export DATABASE_PASSWORD=${servatricePass} && export DATABASE_URL=192.168.0.193 && envsubst < ${servatriceIniTemplatePath} > ${servatriceIniPath}")
+            withCredentials([string(credentialsId: "servatrice_sendgrid_key", variable: "sendgridKey")]){
+                def configReplaceOut = sh(returnStdout: true, script: "export SENDGRID_KEY=${sendgridKey} && export DATABASE_USER=${servatriceUser} && export DATABASE_PASSWORD=${servatricePass} && export DATABASE_URL=192.168.0.193 && envsubst < ${servatriceIniTemplatePath} > ${servatriceIniPath}")
             println configReplaceOut
             def buildout = sh(returnStdout: true, script: "docker build -t ${appName} -f ${dockerfilePathFromRoot} .")
             println buildout
+            }
         }
     }
     stage('push'){
